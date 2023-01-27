6e148ebba4cd5fad833d5b12207d6ce6f1581c2a
mich-man <88162731+surenkov1985@users.noreply.github.com>
2023-01-27 20:47:19 +0300

    Верстка header, верстка слайдера

--- src/index.js
+++ src/index.js
@@ -2,4 +2,42 @@
 
 import "./assets/styles/main.scss";
 
-console.log("hello!!!");
+// Slider
+
+import Swiper from "swiper/bundle";
+
+document.addEventListener("DOMContentLoaded", function () {
+	const NavButton = document.querySelector(".navbar__toggler-btn");
+	const NavbarItem = document.querySelectorAll(".navbar__item");
+
+	NavButton.addEventListener("click", function (e) {
+		NavButton.classList.toggle("active");
+	});
+
+	NavbarItem.forEach((item) => {
+		["mouseover", "mouseout"].forEach((listener) => {
+			item.addEventListener(listener, function (e) {
+				const submenuId = item.dataset.toggle;
+				const Submenu = document.getElementById(submenuId);
+
+				if (e.type === "mouseover") {
+					Submenu.classList.add("show");
+				} else {
+					Submenu.classList.remove("show");
+				}
+			});
+		});
+	});
+
+	const headSlider = new Swiper(".mySlider", {
+		spaceBetween: 20,
+		loop: true,
+		centeredSlides: true,
+		slidesPerView: "auto",
+		// slidesPerView: 1.3,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+	});
+});
