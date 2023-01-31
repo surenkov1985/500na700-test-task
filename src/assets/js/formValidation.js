968c1def3eb2b61186c7bef3a114593f2555e7e9
mich-man <88162731+surenkov1985@users.noreply.github.com>
2023-01-31 17:58:17 +0300

    верстка футера, исправление верстки и стилей хедера, добавлена логика включения меню на мобильных экранах, добавлены валидация полей емайл и телефон

---
+++ src/assets/js/formValidation.js
@@ -0,0 +1,142 @@
+const input = document.querySelectorAll(".input");
+const emailInput = document.querySelector(".email");
+const phoneInput = document.querySelector(".phone");
+
+const inputEvents = ["input", "keydown", "blur"];
+
+inputEvents.forEach((event) => {
+	emailInput.addEventListener(event, function (e) {
+		let key = e.key;
+		let str = e.target.value;
+		let regexp =
+			/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		let testString = "Введите валидный email";
+
+		emailInput.value = str;
+
+		testValue(emailInput, regexp, testString);
+
+		if (key === "Enter" || e.type === "blur") {
+			e.preventDefault();
+			emailInput.dataset.reg = "true";
+
+			testValue(emailInput, regexp, testString);
+
+			if (emailInput.dataset.test === "true") {
+				for (let i = 0; i < input.length; i++) {
+					if (input[i].classList.contains("email")) {
+						input[i + 1].focus();
+					}
+				}
+			}
+		}
+	});
+
+	phoneInput.addEventListener(event, function (e) {
+		let key = e.key;
+		let testReg = /[(\+\d\s\(\d\d\d\)\s\d\d\d-\d\d-\d\d)(\d\s\(\d\d\d\)\s\d\d\d-\d\d-\d\d)]/gi;
+		let valRegRu = /\D/gi;
+		let valRegEur = /\+\d{15}/;
+		let testString = "Введите валидный номер";
+
+		const errElem = document.querySelector(".err-phone");
+
+		let cursorPosition = e.target.selectionStart;
+		let str = formatValueInput(phoneInput, valRegRu);
+		let formatStr = "";
+
+		let rusTel = ["7", "8", "9"];
+
+		if (cursorPosition !== phoneInput.value.length) {
+			if (e.data && valRegRu.test(e.data)) {
+				phoneInput.value = str;
+			}
+
+			return;
+		}
+
+		if (rusTel.indexOf(str[0]) > -1) {
+			if (str[0] === "7") formatStr = "+" + str[0];
+
+			if (str[0] === "8") formatStr = str[0];
+
+			if (str[0] === "9") formatStr = "+7" + str[0];
+
+			if (str.length > 1) {
+				formatStr += " (" + str.slice(1, 4);
+			}
+
+			if (str.length >= 5) {
+				formatStr += ") " + str.slice(4, 7);
+			}
+
+			if (str.length >= 8) {
+				formatStr += "-" + str.slice(7, 9);
+			}
+
+			if (str.length >= 10) {
+				formatStr += "-" + str.slice(9, 11);
+			}
+
+			testValue(phoneInput, testReg, testString);
+		} else {
+			if (str.length >= 1) formatStr = "+" + str;
+
+			testValue(phoneInput, valRegEur, testString);
+		}
+
+		phoneInput.value = formatStr;
+
+		if (key === "Enter" || e.type === "blur") {
+			e.preventDefault();
+
+			phoneInput.dataset.reg = "true";
+
+			if (str[0] === 0 || str[0] < 7) testValue(phoneInput, valRegEur, testString);
+
+			if (str[0] <= 9 && str[0] >= 7) testValue(phoneInput, testReg, testString);
+
+			if (str.length < 10) {
+				errElem.innerHTML = testString;
+				phoneInput.classList.add("error");
+				phoneInput.dataset.test = "false";
+			}
+
+			if (phoneInput.dataset.test === "true") {
+				for (let i = 0; i < input.length; i++) {
+					if (input[i].classList.contains("phone")) {
+						input[i + 1].focus();
+					}
+				}
+			}
+		}
+	});
+});
+
+
+function testValue(elem, reg, string) {
+	if (elem.dataset.reg === "true") {
+		let str = elem.value;
+		const errElem = document.querySelector(`.err-${elem.id}`);
+
+		if (!str.length) {
+			errElem.innerHTML = "Заполните это поле";
+			elem.classList.add("error");
+			elem.dataset.test = "false";
+		} else if (!reg.test(str)) {
+			errElem.innerHTML = string;
+			elem.classList.add("error");
+			elem.dataset.test = "false";
+		} else {
+			elem.classList.remove("error");
+			elem.dataset.test = "true";
+			errElem.innerHTML = "";
+		}
+	}
+}
+
+function formatValueInput(elem, regexp) {
+	let str = elem.value.replace(regexp, "");
+
+	return str;
+}
--- src/index.js
+++ src/index.js
@@ -6,25 +6,108 @@ import "./assets/styles/main.scss";
 
 import Swiper from "swiper/bundle";
 
+// JS import
+
+import "./assets/js/formValidation";
+
 document.addEventListener("DOMContentLoaded", function () {
-	const NavButton = document.querySelector(".navbar__toggler-btn");
-	const NavbarItem = document.querySelectorAll(".navbar__item");
+	const Navbar = document.querySelector(".navbar");
+	const FalseNavbar = document.querySelector(".false-navbar");
+	const FalseBG = document.querySelector(".false__navbar-bg");
+	const FalseNavItem = FalseNavbar.querySelectorAll(".navbar__item");
+	const CloseButton = FalseNavbar.querySelector(".navbar__toggler-btn");
+	const NavButton = Navbar.querySelector(".navbar__toggler-btn");
+	const NavbarItem = Navbar.querySelectorAll(".navbar__item");
+	const NavbarBg = document.querySelector(".navbar__bg");
+	const SubItem = FalseNavbar.querySelectorAll(".navbar__submenu");
+
+	const Questions = document.querySelectorAll(".questions__item-title");
+	const QuestionsSubLists = document.querySelectorAll(".questions__sublist");
+
+	const newsSwiperOptions = {
+		slidesPerView: "auto",
+		freeMode: true,
+	};
+
+	const newsSlider = new Swiper(".news__slider", newsSwiperOptions);
+	const mediaQuery = window.matchMedia("(max-width: 550px)");
+	let mobileWidth = false;
 
 	NavButton.addEventListener("click", function (e) {
-		NavButton.classList.toggle("active");
+		FalseBG.classList.add("show");
+		FalseNavbar.classList.add("active");
+
+		SubItem.forEach((item) => {
+			item.classList.remove("show");
+		});
 	});
 
+	FalseBG.addEventListener("click", function (e) {
+		if (e.target.classList.contains("false__navbar-bg")) {
+			FalseBG.classList.remove("show");
+			FalseNavbar.classList.remove("active");
+		}
+	});
+
+	CloseButton.addEventListener("click", function (e) {
+		FalseBG.classList.remove("show");
+		FalseNavbar.classList.remove("active");
+	});
+
+	mediaChange(mediaQuery);
+
+	mediaQuery.addListener(mediaChange);
+
 	NavbarItem.forEach((item) => {
+		const Link = item.querySelector(".navbar__link");
+		const submenuId = item.dataset.toggle;
+		const Submenu = document.getElementById(submenuId);
+		Submenu.classList.remove("show");
+
 		["mouseover", "mouseout"].forEach((listener) => {
 			item.addEventListener(listener, function (e) {
-				const submenuId = item.dataset.toggle;
-				const Submenu = document.getElementById(submenuId);
-
 				if (e.type === "mouseover") {
 					Submenu.classList.add("show");
+					NavbarBg.classList.add("show");
 				} else {
 					Submenu.classList.remove("show");
+					NavbarBg.classList.remove("show");
 				}
+			});
+		});
+	});
+
+	FalseNavItem.forEach((item) => {
+		const Link = item.querySelector(".navbar__link");
+		const submenuId = item.dataset.toggle;
+		const Submenu = document.getElementById(submenuId);
+
+		["touch", "click"].forEach((listener) => {
+			Link.addEventListener(listener, function (e) {
+				e.preventDefault();
+				SubItem.forEach((item) => {
+					item.classList.remove("show");
+				});
+				const submenuId = item.dataset.toggle;
+				const Submenu = document.getElementById(submenuId);
+				Submenu.classList.add("show");
+			});
+		});
+	});
+
+	Questions.forEach((item) => {
+		const submenuId = item.dataset.toggle;
+		const Submenu = document.getElementById(submenuId);
+
+		["touch", "click"].forEach((listener) => {
+			item.addEventListener(listener, function (e) {
+				e.preventDefault();
+				QuestionsSubLists.forEach((list) => {
+					list.classList.remove("show");
+				});
+				const submenuId = item.dataset.toggle;
+				const Submenu = document.getElementById(submenuId);
+				Submenu.classList.add("show");
 			});
 		});
 	});
@@ -34,10 +117,24 @@ document.addEventListener("DOMContentLoaded", function () {
 		loop: true,
 		centeredSlides: true,
 		slidesPerView: "auto",
-		// slidesPerView: 1.3,
+		initialSlide: 1,
 		autoplay: {
 			delay: 3000,
 			disableOnInteraction: false,
 		},
+		navigation: {
+			prevEl: ".slider__button-prev",
+			nextEl: ".slider__button-next",
+		},
 	});
+
+	function mediaChange(e) {
+		if (e.matches) {
+			newsSlider.enable();
+			mobileWidth = true;
+		} else {
+			newsSlider.disable();
+			mobileWidth = false;
+		}
+	}
 });
